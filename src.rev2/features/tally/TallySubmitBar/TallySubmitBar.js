0cea928db9957818e053f6cc497136da8d6fe093
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-12-14 10:48:50 +0530

    Added the tallyAddition to actionCreator

--- src.rev2/features/tally/TallySubmitBar/TallySubmitBar.js
+++ src.rev2/features/tally/TallySubmitBar/TallySubmitBar.js
@@ -8,6 +8,7 @@ import {addColumn, setRows} from "../../../redux/table/actions";
 import {useDispatch, useSelector} from "react-redux";
 import {exportJsonToExcel} from "../../../components/excel/xlsx/excel";
 import {colToRTCol} from "../../../components/adapters/reactTableAdapter";
+import {addVouchers} from "../../../redux/table/actionCreators";
 
 const TallySubmitBar = () => {
   const dispatch = useDispatch();
@@ -41,37 +42,8 @@ const TallySubmitBar = () => {
       ["Value Date"]: DateToStringDate(item["Value Date"])
     }});
 
-    remoteCall('tally:command:vouchers:add', {tallyTargetCompany, rows: tData})
-        .then((response) => {
-          // console.log(`handleResponse: response=${JSON.stringify(response, null, 2)}`);
-
-          const resultMap = Object.fromEntries(response.map(res => [res.id, res.voucher_id]));
-          // console.log(`resultMap=${JSON.stringify(resultMap)}`);
-
-          // Add column for showing voucherId
-          const tableVoucherCol = columns.filter(col => col.key === 'VoucherId');
-          if (tableVoucherCol.length < 1) {
-            const voucherIdCol = presetColumns.filter(col => col.key === 'VoucherId');
-            if (voucherIdCol.length > 0) {
-              dispatch(addColumn(voucherIdCol[0]));
-            }
-          }
-
-          // Add voucherId
-          const newData = data.map(row => {
-            // console.log(`row=${JSON.stringify(row, null, 2)}`);
-            return {
-              ...row,
-              'VoucherId': resultMap[row.id]
-            }
-          });
-          // console.log(`Saved to Tally: newData=${JSON.stringify(newData, null, 2)}`)
-          dispatch(setRows(newData));
-
-        })
-        .catch(error => {
-          console.error(`handleSubmit: error=${error}`);
-        });
+    console.log(`Added vouchers by thunk`);
+    dispatch(addVouchers(tData, tallyTargetCompany));
   }, []);
 
   const handleSaveClick = useCallback((e) => {
--- src.rev2/redux/table/actionCreators/index.js
+++ src.rev2/redux/table/actionCreators/index.js
@@ -1,6 +1,46 @@
 import * as ActionTypes from '../actionTypes';
 import * as Action from '../actions';
-import {deleteRows, editRows} from "../actions";
+import {addColumn, deleteRows, editRows, setRows} from "../actions";
+import {remoteCall} from "../../../utils/tallyRpc";
+import {presetColumns} from "../../../features/presetColumns";
+
+export const addVouchers = (vouchers, company) => {
+  return async (dispatch, getState) => {
+    remoteCall('tally:command:vouchers:add', {company, rows: vouchers})
+        .then((response) => {
+          // console.log(`handleResponse: response=${JSON.stringify(response, null, 2)}`);
+
+          const resultMap = Object.fromEntries(response.map(res => [res.id, res.voucher_id]));
+          // console.log(`resultMap=${JSON.stringify(resultMap)}`);
+
+          // Add column for showing voucherId
+          const columns = getState().columns;
+          const tableVoucherCol = columns.filter(col => col.key === 'VoucherId');
+          if (tableVoucherCol.length < 1) {
+            const voucherIdCol = presetColumns.filter(col => col.key === 'VoucherId');
+            if (voucherIdCol.length > 0) {
+              dispatch(addColumn(voucherIdCol[0]));
+            }
+          }
+
+          // Add voucherId
+          const data = getState().rows;
+          const newData = data.map(row => {
+            // console.log(`row=${JSON.stringify(row, null, 2)}`);
+            return {
+              ...row,
+              'VoucherId': resultMap[row.id]
+            }
+          });
+          // console.log(`Saved to Tally: newData=${JSON.stringify(newData, null, 2)}`)
+          dispatch(setRows(newData));
+
+        })
+        .catch(error => {
+          console.error(`handleSubmit: error=${error}`);
+        });
+  }
+}
 
 export const deleteVouchers = (ids) => {
   return async (dispatch) => {
