2d6e1b63776abb32c82346aaca163409dbe94399
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-12-19 14:08:28 +0530

    Column view added

--- src.rev2/components/smart-tables/columns/ColumnDetails.js
+++ src.rev2/components/smart-tables/columns/ColumnDetails.js
@@ -1,7 +1,22 @@
-export const ColumnDetails = ({value, onChange}) => {
+export const ColumnDetails = ({value: column, onChange}) => {
   return (
-      <div>
-        {`ColumnDetails: ${JSON.stringify(value, null, 2)}`}
+      <div style={{
+          display: "flex", flexDirection:"column", justifyContent:"center", gap:"10px",
+          padding: "20px"
+        }}
+      >
+        {column && Object.entries(column).map(([key, val], index) => {
+          return (
+              <div key={index} style={{
+                    border: "1px dashed transparent",
+                    display: "flex", flexDirection:"row", justifyContent: "center"
+                  }}
+              >
+                <span style={{border: "1px dashed transparent",width:"30%"}} row={index}>{key}</span>
+                <span style={{border: "1px dashed transparent",width:"30%"}} row={index}>{val}</span>
+              </div>
+          );
+        })}
       </div>
   );
 }
